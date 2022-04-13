277fed75b16e75035902090bdabc1804afa1e2f9
kazi.hassan <kazi.hassan@CRT-13PY903.alexgov.net>
2022-04-13 09:34:52 -0400

    updated

---
+++ src/fahrenheit/App.java
@@ -0,0 +1,29 @@
+package fahrenheit;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		
+		/*
+		convert Fahrenheit to Celsius 
+		
+		// double fahrenheit = 50;
+		
+		// double celsius = (fahrenheit-32) * 5.0/9.0;
+		
+		System.out.println(fahrenheit + " degrees fahrenheit is " +  celsius + " degrees celsius");
+		
+		*/
+		
+		//convert Celsius to Fahrenheit
+		
+		double celsius = 32;
+		
+		double fahrenheit=(celsius*9.0/5.0)+32;
+		
+		System.out.println(celsius + " degrees celsius is " + fahrenheit + " degrees farenheit");
+
+	}
+
+}
